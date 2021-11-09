eb6f10cfe052769e57692f205e76a1fe6a1b6a46
June Bash <junebash@pm.me>
2021-11-09 13:05:04 -0800

    add HLS test

--- VideoTesting/ContentView.swift
+++ VideoTesting/ContentView.swift
@@ -1,17 +1,34 @@
-//
-//  ContentView.swift
-//  VideoTesting
-//
-//  Created by June Bash on 11/9/21.
-//
-
 import SwiftUI
+import AVKit
+import UIKit
 
 struct ContentView: View {
+    @State private var showingPlayer: Bool = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Play") {
+            showingPlayer = true
+        }.sheet(isPresented: $showingPlayer, onDismiss: nil) {
+            VideoPlayer(item: self.item)
+        }
     }
+
+    private var item: AVPlayerItem {
+        let item = AVPlayerItem(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!)
+        return item
+    }
+}
+
+struct VideoPlayer: UIViewControllerRepresentable {
+    let item: AVPlayerItem
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let controller = AVPlayerViewController()
+        controller.player = .init(playerItem: item)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
